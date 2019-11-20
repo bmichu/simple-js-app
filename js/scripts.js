b7243f37bda1719a2c3f6428849a88e3a795064a
bmichu <57345730+bmichu@users.noreply.github.com>
2019-11-20 10:03:40 +0100

    Update scripts.js
    Create Pokemons

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,32 @@
-
-alert('Hello world');
-
-
-
-var favouriteFood = 'pizza';
-document.write(favouriteFood);
+var repository= [
+{
+name:"Venusaur",
+weight:100,
+height:2,
+type:["grass","poison"]
+},
+{
+name:"Ivysaur",
+weight:13,
+height:1,
+type:["grass","poison"]
+},
+{
+name:"Bulbasaur",
+weight:6.9,
+height:0.7,
+type:["grass","poison"]
+},
+{
+name:"Charmeleon",
+weight:19,
+height:1.1,
+type:"fire"
+},
+{
+name:"Wartortle",
+weight:22.5,
+height:1,
+type:"water"
+}
+];

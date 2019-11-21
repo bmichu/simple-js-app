64f82aad0b484ba5c31a769fd1fd625ff676944e
bmichu <57345730+bmichu@users.noreply.github.com>
2019-11-21 08:34:46 +0100

    Update scripts.js
    Creating loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,45 @@
 var repository= [
 {
-name:"Venusaur",
-weight:100,
-height:2,
-type:["grass","poison"]
+  name:"Venusaur",
+  weight:100,
+  height:2,
+  type:["grass","poison"]
 },
 {
-name:"Ivysaur",
-weight:13,
-height:1,
-type:["grass","poison"]
+  name:"Ivysaur",
+  weight:13,
+  height:1,
+  type:["grass","poison"]
 },
 {
-name:"Bulbasaur",
-weight:6.9,
-height:0.7,
-type:["grass","poison"]
+  name:"Bulbasaur",
+  weight:6.9,
+  height:0.7,
+  type:["grass","poison"]
 },
 {
-name:"Charmeleon",
-weight:19,
-height:1.1,
-type:"fire"
+  name:"Charmeleon",
+  weight:19,
+  height:1.1,
+  type:["fire"]
 },
 {
-name:"Wartortle",
-weight:22.5,
-height:1,
-type:"water"
+  name:"Wartortle",
+  weight:22.5,
+  height:1,
+  type:["water"]
 }
-];
+]
+
+
+for(i=0; i< repository.length; i++)
+{
+if (repository[i].height < 1.1 )
+ {
+  document.write(repository[i].name + "(" + repository[i].height + ")") + "<br>");
+ }
+ else
+  {
+   document.write(repository[i].name + "(" + repository[i].height + ")" + "-" + "Wow, that's big!" + "<br>")
+ }
+}
